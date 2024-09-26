687875a178cb5fffe1e52ed28fb090d7f1feb395
ruskjm <rus23008@byui.edu>
2024-09-25 17:23:51 -0700

    latest updates

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,22 +1,53 @@
-import { setLocalStorage } from "./utils.mjs";
+// import { setLocalStorage } from "./utils.mjs";
 
-function productDetailsTemplate(product) {
-  return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
-    <h2 class="divider">${product.NameWithoutBrand}</h2>
-    <img
-      class="divider"
-      src="${product.Image}"
-      alt="${product.NameWithoutBrand}"
-    />
-    <p class="product-card__price">$${product.FinalPrice}</p>
-    <p class="product__color">${product.Colors[0].ColorName}</p>
-    <p class="product__description">
-    ${product.DescriptionHtmlSimple}
-    </p>
-    <div class="product-detail__add">
-      <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-    </div></section>`;
-}
+// function productDetailsTemplate(product) {
+//   return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
+//     <h2 class="divider">${product.NameWithoutBrand}</h2>
+//     <img
+//       class="divider"
+//       src="${product.Image}"
+//       alt="${product.NameWithoutBrand}"
+//     />
+//     <p class="product-card__price">$${product.FinalPrice}</p>
+//     <p class="product__color">${product.Colors[0].ColorName}</p>
+//     <p class="product__description">
+//     ${product.DescriptionHtmlSimple}
+//     </p>
+//     <div class="product-detail__add">
+//       <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+//     </div></section>`;
+// }
+
+// export default class ProductDetails {
+//   constructor(productId, dataSource) {
+//     this.productId = productId;
+//     this.product = {};
+//     this.dataSource = dataSource;
+//   }
+//   async init() {
+//     // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+//     this.product = await this.dataSource.findProductById(this.productId);
+//     // once we have the product details we can render out the HTML
+//     this.renderProductDetails("main");
+//     // once the HTML is rendered we can add a listener to Add to Cart button
+//     // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
+//     document
+//       .getElementById("addToCart")
+//       .addEventListener("click", this.addToCart.bind(this));
+//   }
+//   addToCart() {
+//     setLocalStorage("so-cart", this.product);
+//   }
+//   renderProductDetails(selector) {
+//     const element = document.querySelector(selector);
+//     element.insertAdjacentHTML(
+//       "afterBegin",
+//       productDetailsTemplate(this.product)
+//     );
+//   }
+// }
+
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 
 export default class ProductDetails {
   constructor(productId, dataSource) {
@@ -24,25 +55,41 @@ export default class ProductDetails {
     this.product = {};
     this.dataSource = dataSource;
   }
+
   async init() {
-    // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
     this.product = await this.dataSource.findProductById(this.productId);
-    // once we have the product details we can render out the HTML
-    this.renderProductDetails("main");
-    // once the HTML is rendered we can add a listener to Add to Cart button
-    // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
+    this.renderProductDetails();
     document
       .getElementById("addToCart")
       .addEventListener("click", this.addToCart.bind(this));
   }
+
   addToCart() {
-    setLocalStorage("so-cart", this.product);
+    let cart = getLocalStorage("so-cart");
+    if (!Array.isArray(cart)) {
+      cart = [];
+    }
+    cart.push(this.product);
+    setLocalStorage("so-cart", cart);
   }
-  renderProductDetails(selector) {
-    const element = document.querySelector(selector);
-    element.insertAdjacentHTML(
-      "afterBegin",
-      productDetailsTemplate(this.product)
-    );
+
+  renderProductDetails() {
+    document.querySelector(".product-detail").innerHTML = `
+      <h3>${this.product.Brand.Name}</h3>
+      <h2 class="divider">${this.product.NameWithoutBrand}</h2>
+      <img
+        class="divider"
+        src="${this.product.Image}"
+        alt="${this.product.NameWithoutBrand}"
+      />
+      <p class="product-card__price">$${this.product.FinalPrice}</p>
+      <p class="product__color">${this.product.Colors[0].ColorName}</p>
+      <p class="product__description">
+        ${this.product.DescriptionHtmlSimple}
+      </p>
+      <div class="product-detail__add">
+        <button id="addToCart" data-id="${this.product.Id}">Add to Cart</button>
+      </div>
+    `;
   }
 }
